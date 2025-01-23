a2ebeeb84429eac19a7cd74ca81657174961de06
Bianca Magalhães <bbmsbianca@gmail.com>
2025-01-22 22:19:32 -0300

    add: aula4

---
+++ aula4.rb
@@ -0,0 +1,13 @@
+print "Usuário digite seu nome:"
+
+name = gets.chomp
+
+print "Usuário digite seu sobrenome:"
+
+last_name = gets.chomp
+
+print "Usuário digite sua idade:"
+
+age = gets.chomp
+
+puts "#{name} #{last_name}, #{age}"
